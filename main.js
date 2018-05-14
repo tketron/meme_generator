47447608e10b345374403f2245d68632f9a1d463
Tyler Ketron <tketron@gmail.com>
2018-05-14 15:35:12 -0700

    modified button function to use form inputs

--- main.js
+++ main.js
@@ -1,14 +1,30 @@
 function addMemeDiv() {
   let container = document.getElementById('memes');
   let newDiv = document.createElement('div');
-  newDiv.innerText = "I'm a new Div";
+  let photo = document.getElementById('photo').value;
+  let upperText = document.getElementById('upper').value;
+  let lowerText = document.getElementById('lower').value;
+
+  let upperTextElement = document.createElement('p');
+  upperTextElement.innerText = upperText;
+
+  let lowerTextElement = document.createElement('p');
+  lowerTextElement.innerText = lowerText;
+
+  let photoElement = document.createElement('img');
+  photoElement.setAttribute('src', photo);
+
+  //create elements and append to newdiv
+  newDiv.appendChild(upperTextElement);
+  newDiv.appendChild(photoElement);
+  newDiv.appendChild(lowerTextElement);
+
   container.appendChild(newDiv);
 }
 
 function addBtnListener() {
   let button = document.getElementById('memeBtn');
   button.addEventListener('click', function() {
-    console.log('button clicked');
     addMemeDiv();
   });
 }
